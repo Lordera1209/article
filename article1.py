5a4bed0b38cfa3abf007980eec80ad74b54e2791
Lordera1209 <songzy1209@gmail.com>
2024-07-25 10:52:25 +0800

    Update article1.py

--- article1.py
+++ article1.py
@@ -32,9 +32,9 @@ def sp0(url, k):
     chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
     chrome_options.add_argument('--headless')
     
-    service = Service('./chromedriver_127.0.6533.72_x64.exe')
+    # service = Service('./chromedriver_127.0.6533.72_x64.exe')
     # service = ChromeService(executable_path=ChromeDriverManager().install())
-    driver = webdriver.Chrome(options=chrome_options, service=service)
+    driver = webdriver.Chrome(options=chrome_options)
     
     cookies = {
         'name': 'BAIDUID_BFESS',
