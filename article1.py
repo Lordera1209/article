3aead3aeb6ce5fa80637f5cdc99ebfe091fb28ce
Lordera1209 <songzy1209@gmail.com>
2024-07-25 10:23:16 +0800

    Update article1.py

--- article1.py
+++ article1.py
@@ -33,7 +33,7 @@ def sp0(url, k):
     chrome_options.add_argument('--headless')
     
     # service = Service('./chromedriver_127.0.6533.72_x64.exe')
-    service = ChromeService(executable_path=ChromeDriverManager().install())
+    service = Service(executable_path=ChromeDriverManager().install())
     driver = webdriver.Chrome(options=chrome_options, service=service)
     
     cookies = {
@@ -82,7 +82,7 @@ def sp1(url):
     chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
     chrome_options.add_argument('--headless')
 
-    service = ChromeService(executable_path=ChromeDriverManager().install())
+    service = Service(executable_path=ChromeDriverManager().install())
     # service = Service('./chromedriver_127.0.6533.72_x64.exe')
     driver = webdriver.Chrome(options=chrome_options, service=service)
     
